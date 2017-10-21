29223c03dff1c42109f0fefecda3dc7cd58197c4
linda1019 <32349093+linda1019@users.noreply.github.com>
2017-10-21 11:58:23 -0400

    --revised the random implementation

--- Asg2Q5.java
+++ Asg2Q5.java
@@ -5,9 +5,11 @@ public class Asg2Q5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedListQ5 linkedlist=new LinkedListQ5();
-		for(int i=1;i<=7;i++){
-			linkedlist.add(i);
-		}
+		int number=0;
+		for(int i=1;i<=10;i++){
+			number +=(int) Math.ceil(Math.random() * 50);
+			linkedlist.add(number);
+		}	
 		System.out.println("The original linked list is:");
 		linkedlist.print();
 		System.out.println();
